9ca1785c74b53ac592bfe3dae754a91fbf95e26b
Jeff Goodall <goodalls@live.com>
2023-01-12 20:00:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function clickHandler(e) {
+  switch (e) {
+    case "w":
+      audio("crash");
+      break;
+    case "a":
+      audio("kick-bass");
+      break;
+    case "s":
+      audio("snare");
+      break;
+    case "d":
+      audio("tom-1");
+      break;
+    case "j":
+      audio("tom-2");
+      break;
+    case "k":
+      audio("tom-3");
+      break;
+    case "l":
+      audio("tom-4");
+      break;
+    default:
+      console.log(e);
+      break;
+  }
+}
+
+function audio(drum) {
+  var audio = new Audio("sounds/" + drum + ".mp3");
+  audio.play();
+}
+
+document.querySelectorAll(".drum").forEach(() =>
+  addEventListener("click", (e) => clickHandler(e.target.innerHTML)));
+
+document.querySelectorAll(".drum")
+  .forEach(() => addEventListener("keydown", (e) => clickHandler(e.key)));
